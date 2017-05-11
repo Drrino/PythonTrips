4dd4c9185d233730697f13556f36e08aa702ec87
drrino <q592926752@gmail.com>
2017-05-11 17:49:49 +0800

    some setting

--- AboutUrllib.py
+++ AboutUrllib.py
@@ -1,5 +1,5 @@
-from urllib import parse
 from urllib import request
+from urllib import parse
 
 # 抓取静态网页
 # url_request = request.Request("http://www.baidu.com")
@@ -39,22 +39,44 @@ application/x-www-form-urlencoded ： 浏览器提交 Web 表单时使用
 在使用服务器提供的 RESTful 或 SOAP 服务时， Content-Type 设置错误会导致服务器拒绝服务
 '''
 # 设置Headers
-url = 'https://www.zhihu.com/'
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 ' \
-             'Safari/537.36 '
-values = {'username': 'xxxxxx', 'password': 'xxxxxx'}
-headers = {'User-Agent': user_agent}
-data = parse.urlencode(values)
-headers_request = request.Request(url, data, headers)
-headers_response = request.urlopen(headers_request)
-page = headers_response.read()
+# url = 'https://www.zhihu.com/'
+# user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 ' \
+#              'Safari/537.36 '
+# values = {'username': 'xxxxxx', 'password': 'xxxxxx'}
+# headers = {'User-Agent': user_agent}
+# data = parse.urlencode(values)
+# headers_request = request.Request(url, data, headers)
+# headers_response = request.urlopen(headers_request)
+# page = headers_response.read()
 
 # 代理设置
-enable_proxy = True
-proxy_handler = request.ProxyHandler({"http": 'http://some-proxy.com:8080'})
-null_proxy_handler = request.ProxyHandler({})
-if enable_proxy:
-    opener = request.build_opener(proxy_handler)
-else:
-    opener = request.build_opener(null_proxy_handler)
-request.install_opener(opener)
+# enable_proxy = True
+# proxy_handler = request.ProxyHandler({"http": 'http://some-proxy.com:8080'})
+# null_proxy_handler = request.ProxyHandler({})
+# if enable_proxy:
+#     opener = request.build_opener(proxy_handler)
+# else:
+#     opener = request.build_opener(null_proxy_handler)
+# request.install_opener(opener)
+
+
+# DebugLog
+# httpHandler = request.HTTPHandler(debuglevel=1)
+# httpsHandler = request.HTTPSHandler(debuglevel=1)
+# opener = request.build_opener(httpHandler, httpsHandler)
+# request.install_opener(opener)
+# response = request.urlopen('http://www.baidu.com')
+
+
+
+# URLError
+# req = request.Request('http://blog.csdn.net/cqcre')
+# try:
+#     request.urlopen(req)
+# except request.HTTPError as e:
+#     print(e.code)
+# except request.URLError as e:
+#     print(e.reason)
+# else:
+#     print("OK")
+
