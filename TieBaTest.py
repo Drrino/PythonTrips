cb2449f0b84d0235be82dcf86bdda68fb0e1cd14
drrino <q592926752@gmail.com>
2017-05-19 17:56:02 +0800

    百度贴吧保存成文件

--- TieBaTest.py
+++ TieBaTest.py
@@ -1,6 +1,7 @@
 # 1.对百度贴吧的任意帖子进行抓取
 # 2.指定是否只抓取楼主发帖内容
 # 3.将抓取到的内容分析并保存到文件
+
 import re
 from urllib import request
 
@@ -34,10 +35,18 @@ class Tool:
 
 
 class BDTB:
-    def __init__(self, baseUrl, seeLZ):
+    def __init__(self, baseUrl, seeLZ, floorTag):
         self.baseURL = baseUrl
-        self.seeLZ = '?seeLZ' + str(seeLZ)
+        self.seeLZ = '?see_lz=' + str(seeLZ)
         self.tool = Tool()
+        # 全局file变量,文件写入操作对象
+        self.file = None
+        # 楼层标号
+        self.floor = 1
+        # 默认标题,未获取标题则使用百度贴吧
+        self.defaultTitle = u"百度贴吧"
+        # 是否写入楼分割符标记
+        self.floorTag = floorTag
 
     # 传入页码，获取该页帖子的代码
     def getPage(self, pageNum):
@@ -53,8 +62,7 @@ class BDTB:
                 return None
 
     # 帖子标题
-    def getTitle(self):
-        page = self.getPage(1)
+    def getTitle(self, page):
         pattern = re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>', re.S)
         result = re.search(pattern, page)
         if result:
@@ -64,8 +72,7 @@ class BDTB:
             return None
 
     # 获取帖子总页数
-    def getPageNum(self):
-        page = self.getPage(1)
+    def getPageNum(self, page):
         pattern = re.compile('<li class="l_reply_num.*?</span>.*?<span.*?>(.*?)</span>', re.S)
         result = re.search(pattern, page)
         if result:
@@ -78,13 +85,51 @@ class BDTB:
     def getContent(self, page):
         pattern = re.compile('<div id="post_content_.*?>(.*?)</div>', re.S)
         items = re.findall(pattern, page)
-        floor = 1
+        contents = []
         for item in items:
-            print(floor,u"楼-----------------------------------------------------------------------")
-            print(self.tool.replace(item))
-            floor +=1
+            content = "\n" + self.tool.replace(item) + "\n"
+            contents.append(content)
+        return contents
+
+    def setFileTitle(self, title):
+        if title is not None:
+            self.file = open(title + ".txt", "w+")
+        else:
+            self.file = open(self.defaultTitle + ".txt", "w+")
+
+    def writeData(self, contents):
+        for item in contents:
+            if self.floorTag == '1':
+                # 楼之间的分隔符
+                floorLine = "\n" + str(self.floor) + u"-------------------------------------\n"
+                self.file.write(floorLine)
+            self.file.write(item)
+            self.floor += 1
+
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getPageNum(indexPage)
+        title = self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if pageNum is None:
+            print("URL已失效,请重试")
+            return
+        try:
+            print("该帖子共有" + str(pageNum) + "页")
+            for i in range(1, int(pageNum) + 1):
+                print("正在写入第" + str(i) + "页数据")
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+        except IOError as e:
+            print("写入异常，原因" + e.message)
+        finally:
+            print("写入任务完成")
 
 
-baseURL = 'http://tieba.baidu.com/p/3138733512'
-bdtb = BDTB(baseURL, 1)
-bdtb.getContent(bdtb.getPage(1))
+print(u"请输入帖子代号")
+baseURL = 'http://tieba.baidu.com/p/' + str(input(u'http://tieba.baidu.com/p/'))
+seeLZ = input("是否只获取楼主发言，是输入1，否输入0\n")
+floorTag = input("是否写入楼层信息，是输入1，否输入0\n")
+bdtb = BDTB(baseURL, seeLZ, floorTag)
+bdtb.start()
